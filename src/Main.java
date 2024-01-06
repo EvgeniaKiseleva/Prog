5113888c9ce189660d74c83ee2753a9d68ffa48e
evgeniakiseleva <EvgeniaKiseleva>
2024-01-07 00:55:26 +0300

    Add 1 to 10 for loop printer

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter x value: ");
-        double x = scanner.nextDouble();
-        System.out.println("Please enter y value: ");
-        double y = scanner.nextDouble();
-        if (y < 2 && y > -2 && x < 3 && x > -3) {
-            System.out.println("Orange");
-        } else {
-            System.out.println("White");
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
         }
     }
 }

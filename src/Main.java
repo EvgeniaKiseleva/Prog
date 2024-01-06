4e4c46b06efed3d10b797e1f0d838430a36400a5
evgeniakiseleva <EvgeniaKiseleva>
2024-01-07 00:37:18 +0300

    Print Hello World!

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,6 @@ public class Main {
         double y = scanner.nextDouble();
         if (y < 2 && y > -2 && x < 3 && x > -3) {
             System.out.println("Orange");
-        } else if (y > 3.0 / 5.0 * x - 2) {
-            System.out.println("Yellow");
-        } else if (y < x - 7) {
-            System.out.println("Green");
         } else {
             System.out.println("White");
         }

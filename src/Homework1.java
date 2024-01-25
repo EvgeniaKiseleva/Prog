53bdd8b3b8c38bf7b77020dde6f4193079a5b760
evgeniakiseleva <EvgeniaKiseleva>
2024-01-25 17:16:45 +0300

    Task #3 - in progress

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
 
+
 // Вывести первые 20 чисел фиббоначи, не используя массив
+
 
 public class Homework1 {
     public static void main(String[] args) {
-        int number1 = 0;
-        int number2 = 1;
-
-        System.out.print(number1 + " ");
-
-        for (int i = 1; i <= 20; i++){
-            System.out.print(number2 + " ");
-            int doubleNumber1 = number1;
-            int doubleNumber2 = number2;
-            number1 = doubleNumber2;
-            number2 = doubleNumber1 + doubleNumber2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter x value: ");
+        double x = scanner.nextDouble();
+        System.out.println("Please enter y value: ");
+        double y = scanner.nextDouble();
+        if (y == 0 || x == 0) {
+            System.out.println("black");
+        } else if (y < 2 && y > -2 && x < 3 && x > -3) {
+            System.out.println("Orange");
+        } else if (y > 3.0 / 5.0 * x - 2) {
+            System.out.println("Yellow");
+        } else if (y < x - 7) {
+            System.out.println("Green");
+        } else if (y == 2 || y == -2 || x == 3 || x == -3 || y == 3.0 / 5.0 * x - 2 || y == x - 7) {
+            System.out.println("Blue");
+        } else {
+            System.out.println("White");
         }
     }
 }
